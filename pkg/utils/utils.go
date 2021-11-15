01a906c518ca0a276d62c4451b0a71f2eabf12b3
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-11-15 15:21:14 +0530

    add utils
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

---
+++ pkg/utils/utils.go
@@ -0,0 +1,22 @@
+package utils
+
+import "os"
+
+// IsHyperionChild returns true if the process is running
+// as hyperion child
+//
+// NOTE: The function returns false iff 'HYPERION_CHILD"
+// is set to "false" or else it is assumed that the process
+// is a hyperion child process
+func IsHyperionChild() bool {
+	return os.Getenv("HYPERION_CHILD") != "false"
+}
+
+// ProcFS returns the host machines proc fs location
+func ProcFS() string {
+	if _, err := os.Stat("/hostfs/procfs"); err != nil {
+		return "/proc"
+	}
+
+	return "/hostfs/procfs"
+}
